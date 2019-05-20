f128e60e35b891420c2ee428fe7f60681daa8ff5
aheiting <aheiting@carthage.edu>
2019-05-20 15:30:57 -0500

    Another item in database

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/classes', async(req, res) => {
         const Students = req.body.Students;
 
         await db.insertClass(ClassName, DateTime, Students);
-        //res.redirect('/')
+        res.redirect('/')
     } catch (err) {
         console.log(err);
     }
